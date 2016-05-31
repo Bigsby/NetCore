265913be662436886380735bf87ae4923212dc61
Bigsby Trovalds <bigsby@deb8dev.workgroup>
2016-05-31 15:30:00 +0100

    Add Bigsby Trovalds was here

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApplication
         {
             Console.WriteLine("Hello World, I was created by my father, Bigsby, in an unidentified evironment!");
             Console.WriteLine("Bigsby Gates was here!");
+            Console.WriteLine("Bigsby Trovalds was here!");
         }
     }
 }
